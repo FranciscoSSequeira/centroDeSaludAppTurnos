d369cd3339eb4c183568128047fef7ee8bdc66db
Francisco Sequeira <franciscosequeira879@gmail.com>
2026-01-02 21:09:48 -0300

    agregado server funcionando

--- back/src/server.ts
+++ back/src/server.ts
@@ -8,23 +8,6 @@ const PORT = process.env.PORT || 3000
 
 const server = express();
 
-// Configuración de CORS para permitir solicitudes solo desde el frontend desplegado
-// const allowedOrigins = [
-//     "http://localhost:3000", // Para desarrollo local
-//     "https://centrodesaludappturnos.vercel.app", // Para producción
-// ];
-
-// server.use(cors({
-//     origin: (origin, callback) => {
-//         if (!origin || allowedOrigins.includes(origin)) {
-//             callback(null, true);
-//         } else {
-//             callback(new Error("No permitido por CORS"));
-//         }
-//     },
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-// }));
 server.use(cors({
     origin: "https://centrodesaludappturnos.vercel.app", // Permitir solo el frontend en producción
     methods: ["GET", "POST", "PUT", "DELETE"],
@@ -34,5 +17,9 @@ server.use(express.json());
 server.use(morgan("dev"));
 
 server.use(indexRouter);
+
+server.get("/", (req, res) => {
+    res.send("Servidor funcionando correctamente");
+});
 
 export default server;
